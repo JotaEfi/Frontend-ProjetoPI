08612f30f454b075dbe73776a2b8bff59647558e
JotaEfi <joaodaviabc@gmail.com>
2024-04-29 19:47:06 -0300

    second structural commit

--- app-projeto-pi/src/App.js
+++ app-projeto-pi/src/App.js
@@ -1,5 +1,10 @@
+import Home from "./js/pages/home.js";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ app-projeto-pi/src/js/pages/home.js
@@ -0,0 +1,11 @@
+import "../../styles/index.css";
+import "../../styles/style.css";
+
+function Home() {
+  return (
+    <div className="home_container">
+      <h1>Test1</h1>
+    </div>
+  );
+}
+export default Home;
